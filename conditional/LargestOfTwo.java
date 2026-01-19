dac94079c72269bd3776f8a36eb08a515da7059e
Manoj Maradani <manojmaradani24@gmail.com>
2026-01-19 22:27:28 +0530

    Add EvenOdd program

---
+++ conditional/LargestOfTwo.java
@@ -0,0 +1,15 @@
+package conditional;
+import java.util.Scanner;
+public class LargestOfTwo{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a, b;
+        System.out.println("Enter two numbers:");
+        a = sc.nextInt();
+        b = sc.nextInt();
+        if(a > b)
+            System.out.println(a + " is largest");
+        else
+            System.out.println(b + " is largest");
+    }
+}
