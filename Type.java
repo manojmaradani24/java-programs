eb52157104ce746f83e338925e0fed91d45e3cea
Manoj Maradani <manojmaradani24@gmail.com>
2026-01-20 18:26:54 +0530

    Add Type program

---
+++ Type.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Type {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a character: ");
+        char ch = sc.next().charAt(0);
+        if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
+            System.out.println("Alphabet");
+        } else if (ch >= '0' && ch <= '9') {
+            System.out.println("Digit");
+        } else {
+            System.out.println("Special Symbol");
+        }
+    }
+}
