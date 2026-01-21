2c3349858bef2f2193ff3b45c7d98bacf0c42e97
manoj maradani <manojmaradani24@gmail.com>
2026-01-21 18:19:24 +0530

    Update SwitchCase.java

--- SwitchCase.java
+++ SwitchCase.java
@@ -1,19 +1,13 @@
 import java.util.Scanner;
-
 class Calculator {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-
         System.out.print("Enter first number: ");
         int a = sc.nextInt();
-
         System.out.print("Enter second number: ");
         int b = sc.nextInt();
-
         System.out.print("Enter operator: ");
         char ch = sc.next().charAt(0);
-
         switch (ch) {
             case '+':
                 System.out.println("Answer = " + (a + b));
