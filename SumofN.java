fa6da4687bedb85d078864aae1462f2471ad0a06
Manoj Maradani <manojmaradani24@gmail.com>
2026-01-21 14:34:28 +0530

    Add sum of n numbers program

---
+++ SumofN.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Sum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int n = sc.nextInt();
+        int sum = 0;
+        int i = 1;
+        while (i <= n) {
+            sum = sum + i;
+            i++;
+        }
+        System.out.println("Sum = " + sum);
+    }
+}
