b1ddf9a735150e706bb554fe1d3c394a53e74ab3
Manoj Maradani <manojmaradani24@gmail.com>
2026-01-22 13:25:47 +0530

    Update ReverseNumber program

--- ReverseNumber.java
+++ ReverseNumber.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class ReverseNumber {
+class Reversenumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
