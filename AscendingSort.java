fbfb3890a32ca4c931ba61f9b17362694d3d9ddb
Manoj Maradani <manojmaradani24@gmail.com>
2026-01-30 21:11:12 +0530

    Add AscendingSort program

---
+++ AscendingSort.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class AscendingSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (a[i] > a[j]) {
+                    int temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
+            }
+        }
+        System.out.println("Array in ascending order:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(a[i] + " ");
+        }
+    }
+}
