08f777475f89ed4f0e5a8d78ed54d490f104bf94
Manoj Maradani <manojmaradani24@gmail.com>
2026-01-31 10:38:31 +0530

    Add BubbleSort program

---
+++ BubbleSort.java
@@ -0,0 +1,19 @@
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 1, 4, 2, 8};
+        int temp;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println("Sorted Array:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
