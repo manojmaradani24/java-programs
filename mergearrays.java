98c1b8e0fed8ca58487b264756f97c40ce17b29c
Manoj Maradani <manojmaradani24@gmail.com>
2026-02-01 11:55:18 +0530

    Add mergearrays program

---
+++ mergearrays.java
@@ -0,0 +1,25 @@
+class mergearrays {
+    public static void main(String[] args) {
+        int[] a = {1, 3, 5, 7};
+        int[] b = {2, 4, 6, 8};
+        int[] c = new int[a.length + b.length];
+        int i = 0, j = 0, k = 0;
+        while (i < a.length && j < b.length) {
+            if (a[i] < b[j]) {
+                c[k++] = a[i++];
+            } else {
+                c[k++] = b[j++];
+            }
+        }
+        while (i < a.length) {
+            c[k++] = a[i++];
+        }
+        while (j < b.length) {
+            c[k++] = b[j++];
+        }
+        System.out.println("Merged Array:");
+        for (int x : c) {
+            System.out.print(x + " ");
+        }
+    }
+}
