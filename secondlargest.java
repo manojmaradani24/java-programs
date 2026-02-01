9128c542f8dd87611c77ccf22dc9726e39c7d5b9
Manoj Maradani <manojmaradani24@gmail.com>
2026-02-01 12:35:00 +0530

    Add secondlargest program

---
+++ secondlargest.java
@@ -0,0 +1,16 @@
+class secondlargest {
+    public static void main(String[] args) {
+        int[] arr = {10, 5, 20, 8, 15};
+        int largest = Integer.MIN_VALUE;
+        int second = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > largest) {
+                second = largest;
+                largest = arr[i];
+            } else if (arr[i] > second && arr[i] != largest) {
+                second = arr[i];
+            }
+        }
+        System.out.println("Second Largest = " + second);
+    }
+}
