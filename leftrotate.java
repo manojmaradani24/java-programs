b044c440078e3c1824350728cbefbd031177258f
Manoj Maradani <manojmaradani24@gmail.com>
2026-02-02 14:03:47 +0530

    Add leftrotate program

---
+++ leftrotate.java
@@ -0,0 +1,13 @@
+class leftrotate {
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3, 4, 5};
+        int temp = a[0];
+        for (int i = 0; i < a.length - 1; i++) {
+            a[i] = a[i + 1];
+        }
+        a[a.length - 1] = temp;
+        for (int x : a) {
+            System.out.print(x + " ");
+        }
+    }
+}
