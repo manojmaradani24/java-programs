2864c39615e39ad5578a9935cf0eede0ebf6ee0e
Manoj Maradani <manojmaradani24@gmail.com>
2026-02-02 14:14:32 +0530

    Add stringlength program

---
+++ stringlength.java
@@ -0,0 +1,10 @@
+class stringlength {
+    public static void main(String[] args) {
+        String str = "Hello";
+        int count = 0;
+        for (char c : str.toCharArray()) {
+            count++;
+        }
+        System.out.println(count);
+    }
+}
