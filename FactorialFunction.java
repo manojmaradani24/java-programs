db848f37d7be1dbad19f5d470fa3605ab49f6a2e
Manoj Maradani <manojmaradani24@gmail.com>
2026-02-05 14:14:22 +0530

    Add FactorialFunction program

---
+++ FactorialFunction.java
@@ -0,0 +1,13 @@
+public class FactorialFunction {
+    static int factorial(int n) {
+        int fact = 1;
+        for (int i = 1; i <= n; i++) {
+            fact = fact * i;
+        }
+        return fact;
+    }
+    public static void main(String[] args) {
+        int num = 5;
+        System.out.println("Factorial of " + num + " is " + factorial(num));
+    }
+}
