86af6419d14062008bcd6df991558c808dd35c5c
Manoj Maradani <manojmaradani24@gmail.com>
2026-02-05 14:37:02 +0530

    Add RecursiveFactorial program

---
+++ RecursiveFactorial.java
@@ -0,0 +1,12 @@
+public class RecursiveFactorial {
+    static int factorial(int n) {
+        if (n == 0 || n == 1)
+            return 1;
+        else
+            return n * factorial(n - 1);
+    }
+    public static void main(String[] args) {
+        int num = 5;
+        System.out.println("Factorial of " + num + " is " + factorial(num));
+    }
+}
