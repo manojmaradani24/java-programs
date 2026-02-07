9922135023c292004040d945a366915d2edcbc6a
Manoj Maradani <manojmaradani24@gmail.com>
2026-02-07 13:26:59 +0530

    Add FunctionWithReturn program

---
+++ FunctionWithReturn.java
@@ -0,0 +1,9 @@
+class FunctionWithReturn {
+    static int add(int a, int b) {
+        return a + b;
+    }
+    public static void main(String[] args) {
+        int result = add(5, 3);
+        System.out.println("Sum = " + result);
+    }
+}
