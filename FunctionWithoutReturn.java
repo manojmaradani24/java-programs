ba48a4b3f305b2081dcd9bcaead7a601a6a28846
Manoj Maradani <manojmaradani24@gmail.com>
2026-02-07 13:32:42 +0530

    Add FunctionWithoutReturn program

---
+++ FunctionWithoutReturn.java
@@ -0,0 +1,8 @@
+class FunctionWithoutReturn {
+    static void greet() {
+        System.out.println("Hello, Welcome to Java Functions!");
+    }
+    public static void main(String[] args) {
+        greet();
+    }
+}
