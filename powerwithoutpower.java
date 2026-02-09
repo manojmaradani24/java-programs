1d5e7b4685cc85e7395922473c9b5bbb3f6bf12e
Manoj Maradani <manojmaradani24@gmail.com>
2026-02-09 20:24:18 +0530

    Add powerwithoutpower program

---
+++ powerwithoutpower.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class powerwithoutpower {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter base: ");
+        int base = sc.nextInt();
+        System.out.print("Enter exponent: ");
+        int exp = sc.nextInt();
+        int result = 1;
+        for (int i = 1; i <= exp; i++) {
+            result = result * base;
+        }
+        System.out.println(base + " power " + exp + " = " + result);
+    }
+}
